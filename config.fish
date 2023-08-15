37a7e51dcdc2c2a4c5376279fb154e81b880294d
Gabe Meola <gabriel.meola@discordapp.com>
2023-08-15 11:24:35 -0600

    use bass to source profile

--- config.fish
+++ config.fish
@@ -1,4 +1,11 @@
 # Source .profile if exists
 if test -f ~/.profile
-    source ~/.profile;
+    bass source ~/.profile;
+end
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+function __tabby_working_directory_reporting --on-event fish_prompt
+    echo -en "\e]1337;CurrentDir=$PWD\x7"
 end
