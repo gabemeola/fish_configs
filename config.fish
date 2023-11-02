14d8e7053a314178db3fb9e702c7230a18626284
Gabe Meola <gabriel.meola@discordapp.com>
2023-11-01 18:33:23 -0600

    add pnpm auto completes

--- config.fish
+++ config.fish
@@ -9,3 +9,7 @@ set --export PATH $BUN_INSTALL/bin $PATH
 function __tabby_working_directory_reporting --on-event fish_prompt
     echo -en "\e]1337;CurrentDir=$PWD\x7"
 end
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
