517618bed36646e80e312acd325d25ec92c6b0f3
Gabe M <hey@gabe.mx>
2024-12-26 21:54:25 -0700

    block cursor

--- config.fish
+++ config.fish
@@ -9,3 +9,5 @@ set --export PATH $BUN_INSTALL/bin $PATH
 function __tabby_working_directory_reporting --on-event fish_prompt
     echo -en "\e]1337;CurrentDir=$PWD\x7"
 end
+
+set fish_cursor_default block
