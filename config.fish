55aa0734c29af5820eb15bf4a90652bdfff7f61e
Gabe M <hey@gabe.mx>
2025-02-24 08:26:01 -0700

    source brew completions

--- config.fish
+++ config.fish
@@ -9,3 +9,12 @@ set --export PATH $BUN_INSTALL/bin $PATH
 function __tabby_working_directory_reporting --on-event fish_prompt
     echo -en "\e]1337;CurrentDir=$PWD\x7"
 end
+
+# Source fish completions
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
